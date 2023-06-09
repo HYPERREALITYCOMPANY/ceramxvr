a3cd05dacc8c27420e2160f2ff756da4c95ffe64
Eduardo-45 <115485379+Alan4745@users.noreply.github.com>
2023-06-09 17:10:26 -0600

    change 09/06/2023

--- js/script.js
+++ js/script.js
@@ -1,20 +1,71 @@
-window.onload = function() {
-  var barra = document.querySelector('.barra');
-  var texto = document.querySelector('.texto');
-  var ascene = document.querySelector('#ascene');
+let test = false;
+
+window.onload = function () {
+  var barra = document.querySelector(".barra");
+  var texto = document.querySelector(".texto");
+  var ascene = document.querySelector("#ascene");
+  var screenStart = document.querySelector("#screenStart");
+  var audioMainMenu = document.querySelector("#audioMainMenu");
+  var contenedorBarra = document.querySelector("#contenedorBarra");
+  var textStart = document.querySelector("#textStart");
+  var pared = document.querySelector("#pared");
+  let test1 = false;
+
+  if (screenStart) {
+    screenStart.addEventListener("click", function () {
+      if (test1) {
+        ascene.setAttribute("visible", "true");
+        document.querySelector(".contenedor").style.display = "none";
+        setTimeout(() => {
+          pared.setAttribute("position", "0 25 2");
+        }, 3000);
+        var test = localStorage.getItem("audioMainMenu");
+        console.log(test, "test");
+
+        if (
+          localStorage.getItem("audioMainMenu") !== null ||
+          localStorage.getItem("audioBackgroud") !== null ||
+          localStorage.getItem("audioVoicer") !== null ||
+          localStorage.getItem("videoAudio") !== null
+        ) {
+          console.log("verdadero");
+          audioMainMenu.play();
+          audioMainMenu.volume =
+            parseInt(localStorage.getItem("audioMainMenu")) / 100;
+
+          console.log(parseInt(localStorage.getItem("audioMainMenu")) / 100);
+        } else {
+          console.log("falso");
+
+          localStorage.setItem("audioMainMenu", "50");
+          localStorage.setItem("audioBackgroud", "20");
+          localStorage.setItem("audioVoicer", "80");
+          localStorage.setItem("videoAudio", "60");
+
+          audioMainMenu.play();
+          audioMainMenu.volume =
+            parseInt(localStorage.getItem("audioMainMenu")) / 100;
+        }
+      }
+    });
+  }
 
   var porcentaje = 0;
-  var intervalo = setInterval(function() {
-      if (porcentaje >= 100) {
-          clearInterval(intervalo);
-          setTimeout(function() {
-            ascene.setAttribute('visible', 'true')
-            document.querySelector('.contenedor').style.display = 'none';
-          }, 500);
-      } else {
-          porcentaje += 1;
-          barra.style.width = porcentaje + '%';
-          texto.innerHTML = 'Loading ' + porcentaje + '%';
-      }
+  var intervalo = setInterval(function () {
+    if (porcentaje >= 100) {
+      clearInterval(intervalo);
+      setTimeout(function () {
+        contenedorBarra.setAttribute("style", "display: none");
+        textStart.setAttribute(
+          "style",
+          "display: block; margin-top: 50px; margin-bottom: 0px;"
+        );
+        test1 = true;
+      }, 500);
+    } else {
+      porcentaje += 1;
+      barra.style.width = porcentaje + "%";
+      texto.innerHTML = "Loading " + porcentaje + "%";
+    }
   }, 20);
-}
+};
